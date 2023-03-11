5dbadb686fcb09c3c52f529c1106f8a970d119ad
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-03-11 17:47:55 +0400

    Added PaymentRequest

---
+++ ms-payment/src/main/java/com/company/mspayment/model/request/PaymentRequest.java
@@ -0,0 +1,19 @@
+package com.company.mspayment.model.request;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class PaymentRequest {
+    BigDecimal amount;
+    String description;
+    String currency;
+}
