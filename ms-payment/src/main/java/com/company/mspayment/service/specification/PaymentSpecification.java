cc5e93a4d5433b8caea91d301f970751f6c2fb4a
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-03-13 23:32:48 +0400

    Fixed

--- ms-payment/src/main/java/com/company/mspayment/service/specification/PaymentSpecification.java
+++ ms-payment/src/main/java/com/company/mspayment/service/specification/PaymentSpecification.java
@@ -1,4 +1,25 @@
 package com.company.mspayment.service.specification;
 
-public class PaymentSpecification {
+import com.company.mspayment.entity.Payment;
+import com.company.mspayment.model.request.PaymentCriteria;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+@RequiredArgsConstructor
+public class PaymentSpecification implements Specification<Payment> {
+
+    private final PaymentCriteria paymentCriteria;
+
+    private static final String AMOUNT="amount";
+    private static final String DESCRIPTION="description";
+
+    @Override
+    public Predicate toPredicate(Root<Payment> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+        return null;
+    }
 }
