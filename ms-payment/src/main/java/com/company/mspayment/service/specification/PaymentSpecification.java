c151ea03fd1ab05220e51fabdb9e888b24d39219
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-03-14 23:24:29 +0400

    Fixed

--- ms-payment/src/main/java/com/company/mspayment/service/specification/PaymentSpecification.java
+++ ms-payment/src/main/java/com/company/mspayment/service/specification/PaymentSpecification.java
@@ -4,22 +4,45 @@ import com.company.mspayment.entity.Payment;
 import com.company.mspayment.model.request.PaymentCriteria;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 public class PaymentSpecification implements Specification<Payment> {
 
     private final PaymentCriteria paymentCriteria;
 
-    private static final String AMOUNT="amount";
-    private static final String DESCRIPTION="description";
+    private static final String AMOUNT = "amount";
+    private static final String DESCRIPTION = "description";
 
     @Override
     public Predicate toPredicate(Root<Payment> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
-        return null;
+
+        List<Predicate> predicates = new ArrayList<>();
+        if (predicates != null) {
+            if (paymentCriteria.getAmountFrom() != null) {
+                predicates.add(
+                        criteriaBuilder.greaterThanOrEqualTo(root.get(AMOUNT), paymentCriteria.getAmountFrom()));
+            }
+
+            if (paymentCriteria.getAmountFrom() != null) {
+                predicates
+                        .add(
+                                criteriaBuilder.lessThanOrEqualTo(root.get(AMOUNT), paymentCriteria.getAmountFrom()));
+            }
+
+            if (StringUtils.hasText(paymentCriteria.getDescription())) {
+                predicates.add(
+                        criteriaBuilder.like(root.get(DESCRIPTION), "%" + paymentCriteria.getDescription() + "%"));
+            }
+        }
+        return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+
     }
 }
