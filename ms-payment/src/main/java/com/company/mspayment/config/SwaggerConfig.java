c361904a059bd94f5432146a6cb4a11d70ae0ed5
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-03-17 23:28:32 +0400

    Added SwaggerConfig

--- ms-payment/src/main/java/com/company/mspayment/config/SwaggerConfig.java
+++ ms-payment/src/main/java/com/company/mspayment/config/SwaggerConfig.java
@@ -1,7 +1,20 @@
 package com.company.mspayment.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
+@EnableSwagger2
 @Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("az.company.mspayment"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
 }
